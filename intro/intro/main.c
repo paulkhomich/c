4bbc4d2a55bcd85b7532703b94dffe1513222de7
Paul Khomich <paulkhomich@outlook.com>
2019-09-06 18:58:22 +0500

    intro

---
+++ intro/intro/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  intro
+//
+//  Created by Павел Хомич on 06/09/2019.
+//  Copyright © 2019 Павел Хомич. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, intro!\n");
+    return 0;
+}
